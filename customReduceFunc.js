d2c3615e8dbf6e7a41310ea23b8d710faefa1bf4
Rajesh Jha <jharajesh035@gmail.com>
2025-03-13 00:31:28 +0530

    feat: Update reduce polyfill

--- customReduceFunc.js
+++ customReduceFunc.js
@@ -15,24 +15,39 @@
 
 // console.log(result)
 
-Array.prototype.customReduceFunc = function(...args) {
-    const hasInitialValue = args.length > 1;
+// Array.prototype.customReduceFunc = function(...args) {
+//     const hasInitialValue = args.length > 1;
     
-    if(!hasInitialValue && this.length === 0) {
+//     if(!hasInitialValue && this.length === 0) {
+//         throw new Error('Reduce of empty array with no initial value');
+//     }
+
+//     let result = hasInitialValue ? args[1] : this[0];
+
+//     for(let i=hasInitialValue ? 0 : 1; i<this.length; i++) {
+//         result = args[0](result, this[i], i, this);
+//     }
+
+//     return result;
+// }
+
+Array.prototype.customReduceFunc = function(cb, initialValue) {
+    const hasInitialValue = arguments.length > 1;
+    
+    if (!hasInitialValue && this.length === 0) {
         throw new Error('Reduce of empty array with no initial value');
     }
 
-    let result = hasInitialValue ? args[1] : this[0];
+    let accumulator = hasInitialValue ? initialValue : this[0];
 
-    for(let i=hasInitialValue ? 0 : 1; i<this.length; i++) {
-        result = args[0](result, this[i], i, this);
+    for (let i = hasInitialValue ? 0 : 1; i < this.length; i++) {
+        accumulator = cb(accumulator, this[i], i, this);
     }
 
-    return result;
-}
+    return accumulator;
+};
 
-const result = [1, 2, 3].reduce((acc, currentValue) => {
-    console.log(acc, currentValue);
+const result = [1, 2, 3, 4].customReduceFunc((acc, currentValue) => {
     return acc + currentValue;
 });
 
