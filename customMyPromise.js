c6e6f6f3d8fffac227b7c3ebdfd55ade3f49e160
Rajesh Jha <jharajesh035@gmail.com>
2025-03-18 23:10:59 +0530

    feat: feat: implement custom Promise with then, catch, and static methods

---
+++ customMyPromise.js
@@ -0,0 +1,151 @@
+// new Promise((resolve, reject))
+// new Promise((resolve, reject)).then(data => console.log(data)).then(data => console.log(data))
+// new Promise((resolve, reject)).catch(err => console.log(err)).then(err => console.log(err))
+// Promise.resolve(data);
+// Promise.reject(error);
+// new Promise((resolve, reject)).then(data => return new Promise()).then(data => console.log(data))
+
+// #status: Pending | fulfilled | rejected
+// DataStructure to maintain the chain of #handlers:- #handlers<Array> = [handler, handler]
+
+const PROMISE_STATUS = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+};
+
+class MyPromise {
+  #status;
+  #result;
+  #isResolveOrRejectCalled;
+  #handlers;
+
+  constructor(executor) {
+    this.#status = PROMISE_STATUS.PENDING; //maintain the #status
+    this.#handlers = []; //maintain instance variables of handle
+    this.#isResolveOrRejectCalled = false;
+    this.#result = undefined;
+
+    //The resolve and reject when they called as part of executor have the appropraite context.
+    // Which is current context of promise being passed to it.
+    //Call the executor by passing it the resolve and reject
+    try {
+      executor(this.#resolve.bind(this), this.#reject.bind(this));
+    } catch (e) {
+      this.#reject(e);
+    }
+  }
+
+  #resolve(value) {
+    if (this.#isResolveOrRejectCalled) return;
+
+    this.#isResolveOrRejectCalled = true;
+
+    if (value instanceof MyPromise) {
+      value.then((v) => {
+        this.#status = PROMISE_STATUS.FULFILLED;
+        this.#result = v;
+        this.#executehandlers();
+      });
+      return;
+    }
+
+    this.#status = PROMISE_STATUS.FULFILLED;
+    this.#result = value;
+    this.#executehandlers();
+  }
+
+  #reject(error) {
+    if (this.#isResolveOrRejectCalled) return;
+    this.#isResolveOrRejectCalled = true;
+
+    this.#status = PROMISE_STATUS.REJECTED;
+    this.#result = error;
+    this.#executehandlers();
+  }
+
+  then(onFulfilled, onRejected) {
+    const handler = {
+      onFulfilled:
+        typeof onFulfilled === "function" ? onFulfilled : (value) => value,
+      onRejected:
+        typeof onRejected === "function"
+          ? onRejected
+          : (reason) => {
+              throw reason;
+            },
+    };
+    const promise = new MyPromise((resolve, reject) => {
+      handler.resolve = resolve;
+      handler.reject = reject;
+    });
+
+    this.#handlers.push(handler);
+
+    if (this.#status !== PROMISE_STATUS.PENDING) {
+      this.#executehandlers();
+    }
+
+    return promise;
+  }
+
+  #executehandlers() {
+    for (const { onFulfilled, onRejected, resolve, reject } of this.#handlers) {
+      const handler =
+        this.#status === PROMISE_STATUS.FULFILLED ? onFulfilled : onRejected;
+      queueMicrotask(() => {
+        if (typeof handler !== "function") {
+          return;
+        }
+
+        try {
+          const returnValue = handler(this.#result);
+          if (!(returnValue instanceof MyPromise)) {
+            resolve(returnValue);
+          } else {
+            returnValue.then(resolve, reject);
+          }
+        } catch (error) {
+          if (typeof reject === "function") {
+            reject(error);
+          }
+        }
+      });
+    }
+
+    this.#handlers = [];
+  }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected);
+  }
+
+  static resolve(value) {
+    if (value instanceof MyPromise) return value;
+    return new MyPromise((resolve) => {
+      resolve(value);
+    });
+  }
+  static reject(reason) {
+    return new MyPromise((_, reject) => {
+      reject(reason);
+    });
+  }
+}
+
+const mp1 = new MyPromise((resolve, reject) => {
+  setTimeout(() => resolve(1), 100);
+});
+const mp2 = MyPromise.resolve(2);
+const mp = new MyPromise((resolve, reject) => {
+  resolve(mp1);
+  resolve(mp2);
+  reject(3);
+}).then(
+  (data) => {
+    console.log("resolve", data);
+  },
+  (reason) => {
+    console.log("reject", reason);
+  }
+);
