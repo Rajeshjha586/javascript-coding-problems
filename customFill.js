099a407226790529e7d4c74063d3d91a53ede92d
Rajesh Jha <jharajesh035@gmail.com>
2025-04-28 22:54:14 +0530

    feat: Update ployfill for Array.prototype.fill

--- customFill.js
+++ customFill.js
@@ -32,27 +32,44 @@ Array.prototype.customFill = function (value, start = 0, end = this.length) {
   return this;
 };
 
-const array1 = [1, 2, 3, 4];
-// Fill with 0 from position 2 until position 4
-console.log(array1.customFill(0, 2, 4));
-// Expected output: Array [1, 2, 0, 0]
-// Fill with 5 from position 1
-console.log(array1.customFill(5, 1));
-// Expected output: Array [1, 5, 5, 5]
-console.log(array1.customFill(6));
-// Expected output: Array [6, 6, 6, 6]
+// const array1 = [1, 2, 3, 4];
+// // Fill with 0 from position 2 until position 4
+// console.log(array1.customFill(0, 2, 4));
+// // Expected output: Array [1, 2, 0, 0]
+// // Fill with 5 from position 1
+// console.log(array1.customFill(5, 1));
+// // Expected output: Array [1, 5, 5, 5]
+// console.log(array1.customFill(6));
+// // Expected output: Array [6, 6, 6, 6]
 
-console.log([1, 2, 3].customFill(4)); // [4, 4, 4]
-console.log([1, 2, 3].customFill(4, 1)); // [1, 4, 4]
-console.log([1, 2, 3].customFill(4, 1, 2)); // [1, 4, 3]
-console.log([1, 2, 3].customFill(4, 1, 1)); // [1, 2, 3]
-console.log([1, 2, 3].customFill(4, 3, 3)); // [1, 2, 3]
-console.log([1, 2, 3].customFill(4, -3, -2)); // [4, 2, 3]
-console.log([1, 2, 3].customFill(4, NaN, NaN)); // [1, 2, 3]
-console.log([1, 2, 3].customFill(4, 3, 5)); // [1, 2, 3]
-console.log(Array(3).customFill(4)); // [4, 4, 4]
+// console.log([1, 2, 3].customFill(4)); // [4, 4, 4]
+// console.log([1, 2, 3].customFill(4, 1)); // [1, 4, 4]
+// console.log([1, 2, 3].customFill(4, 1, 2)); // [1, 4, 3]
+// console.log([1, 2, 3].customFill(4, 1, 1)); // [1, 2, 3]
+// console.log([1, 2, 3].customFill(4, 3, 3)); // [1, 2, 3]
+// console.log([1, 2, 3].customFill(4, -3, -2)); // [4, 2, 3]
+// console.log([1, 2, 3].customFill(4, NaN, NaN)); // [1, 2, 3]
+// console.log([1, 2, 3].customFill(4, 3, 5)); // [1, 2, 3]
+// console.log(Array(3).customFill(4)); // [4, 4, 4]
 
-// A single object, referenced by each slot of the array:
-const arr = Array(3).customFill({}); // [{}, {}, {}]
-arr[0].hi = "hi"; // [{ hi: "hi" }, { hi: "hi" }, { hi: "hi" }]
-console.log(arr);
+// // A single object, referenced by each slot of the array:
+// const arr = Array(3).customFill({}); // [{}, {}, {}]
+// arr[0].hi = "hi"; // [{ hi: "hi" }, { hi: "hi" }, { hi: "hi" }]
+// console.log(arr);
+
+const arr = new Array(3);
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = new Array(4).customFill(1); // Creating an array of size 4 and filled of 1
+}
+
+arr[0][0] = 10;
+console.log(arr[0][0]); // 10
+console.log(arr[1][0]); // 1
+console.log(arr[2][0]); // 1
+
+const tempGirls = Array(5).customFill("girl", 0);
+console.log(tempGirls);
+
+const arrayLike = { length: 2 };
+console.log(Array.prototype.customFill.call(arrayLike, 1));
+// { '0': 1, '1': 1, length: 2 }
